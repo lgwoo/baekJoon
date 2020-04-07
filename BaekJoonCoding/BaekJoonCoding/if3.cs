2638b50f8eaf11f6efff20ef39017b2f074323a2
건우 이 <ggg0811@daum.net>
2020-04-07 23:56:42 +0900

    백준코딩 if문 "윤년 확인"

---
+++ BaekJoonCoding/BaekJoonCoding/if3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaekJoonCoding
+{
+    class if3
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if (((a % 4 == 0) && (a % 100 != 0)) || (a % 400 == 0))
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+
+            }
+        }
+    }
+}
