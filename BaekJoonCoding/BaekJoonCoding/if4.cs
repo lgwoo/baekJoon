28895ff2c509d471937e168ead0a3bd60c0c80fb
건우 이 <ggg0811@daum.net>
2020-04-08 00:10:25 +0900

    제 1,2,3,4분면 확인

---
+++ BaekJoonCoding/BaekJoonCoding/if4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaekJoonCoding
+{
+    class if4
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > 0)
+            {
+                if (b > 0)
+                {
+                    Console.WriteLine(1);
+                }
+                else { Console.WriteLine(4); }
+            }
+            else
+            {
+                if (b > 0)
+                {
+                    Console.WriteLine(2);
+                }
+                else { Console.WriteLine(3); }
+            }
+        }
+    }
+}
