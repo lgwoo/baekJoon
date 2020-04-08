7cc69078ebeb73c571c9768e219925e1eeffd63b
이건우 <ggg0811@daum.net>
2020-04-08 13:33:45 +0900

    시간 45분전표시

---
+++ BaekJoonCoding/BaekJoonCoding/if5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaekJoonCoding
+{
+    class if5
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (b >= 45)
+            {
+                b -= 45;
+            }
+            else if (a > 0)
+            {
+                a -= 1; b += 15;
+            }
+            else
+            {
+                a = 23; b += 15;
+            }
+            Console.WriteLine(a , b);
+        }
+    }
+}
