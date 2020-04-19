1fc19b89b794e36888e42e39b839f3f3a1d4e669
이건우 <ggg0811@daum.net>
2020-04-19 14:36:05 +0900

    .

---
+++ BaekJoonCoding/BaekJoonCoding/while1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaekJoonCoding
+{
+    class while1
+    {
+        static void Main()
+        {
+            bool a = true;
+            var bilder = new StringBuilder();
+            while (a)
+            {
+                string[] n = Console.ReadLine().Split();
+                int x = int.Parse(n[0]);
+                int y = int.Parse(n[1]);
+                if (x == 0 && y == 0)
+                {
+                    a = false;
+                }
+                else  {Console.WriteLine(x + y);
+}
+            }
+        }
+    }
+}
---
+++ BaekJoonCoding/BaekJoonCoding/while2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaekJoonCoding
+{
+    class while2
+    {
+        static void Main()
+        {
+            for (; ; )
+            {
+                string s = Console.ReadLine();
+                if (s == null)
+                {
+                    break;
+                }
+                string[] ss = s.Split();
+                int a = int.Parse(ss[0]);
+                int b = int.Parse(ss[1]);
+                Console.WriteLine(a + b);
+            }
+        }
+    }
+
+}
+ 
